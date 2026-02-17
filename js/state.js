ebb5b028ff6565e1b95c027f7320fe45d305eef4
Nobe <welcome.nobe@gmail.com>
2026-02-18 00:15:05 +0100

    Refactor params to use var and update finishDiameter

--- js/state.js
+++ js/state.js
@@ -1,20 +1,12 @@
-export const params = {
-    // Dimensions
+var params = {
     height: 320,
     diameter: 75,
     bodyHeight: 200,
-    
-    // Pied
     baseRadius: 5,
-    
-    // Épaule & Col
     shoulderCurve: 40,
     neckHeight: 270,
     neckCurve: 20,
     neckTopDiameter: 29,
-    neckBottomDiameter: 30, // Ajouté car présent dans ton HTML
-    
-    // Bague
     finishHeight: 15,
-    finishDiameter: 29.6
+    finishDiameter: 30
 };
