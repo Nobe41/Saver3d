f2ba1efbd3203c2d9f1e3f5d7b7eda0517a87188
Nobe <welcome.nobe@gmail.com>
2026-02-18 22:06:57 +0100

    Remove DOMContentLoaded event listener

--- js/main.js
+++ js/main.js
@@ -1,12 +1,1 @@
-// On attend que la page soit prête
-window.addEventListener('DOMContentLoaded', () => {
-    console.log("Atelier Bouteille - Démarrage...");
-    
-    // Initialiser l'interface
-    initUI();
-    
-    // Initialiser la 3D (après un court délai pour être sûr)
-    setTimeout(() => {
-        initViewer();
-    }, 100);
-});
+console.log("Atelier Bouteille chargé avec succès.");
