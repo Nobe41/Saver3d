314391ac591b5983494c2e0b91d2f7d914f9233b
Nobe <welcome.nobe@gmail.com>
2026-02-18 10:29:33 +0100

    new state js

--- js/state.js
+++ js/state.js
@@ -1,12 +1,20 @@
+// DÉFINITION DES PARAMÈTRES PAR DÉFAUT
 var params = {
+    // Dimensions principales
     height: 320,
     diameter: 75,
     bodyHeight: 200,
-    baseRadius: 5,
+    
+    // Épaule
     shoulderCurve: 40,
-    neckHeight: 270,
-    neckCurve: 20,
-    neckTopDiameter: 29,
+    
+    // Col
+    neckHeight: 270,   // Hauteur où commence la partie verticale du col
+    neckDiameter: 29,  // Diamètre externe du col
+    
+    // Bague
     finishHeight: 15,
-    finishDiameter: 30
+    
+    // Pied
+    baseRadius: 5
 };
