b6dfd28f721a18655d918004e253d1d4eb8eff25
Nobe <welcome.nobe@gmail.com>
2026-02-19 13:56:45 +0100

    Update state.js

--- js/state.js
+++ js/state.js
@@ -2,5 +2,5 @@
 var scene, camera, renderer, controls, bottleGroup;
 var viewport3D;
 
-// NOUVEAU : On prépare la mémoire pour les gravures et la police 3D
-var font3D = null;
+// Mémoire pour stocker les images (PNG) des gravures
+window.engravingImages = {};
