bcb40ef8a54e5c6cadfaf7c242564c899905747f
Nobe <welcome.nobe@gmail.com>
2026-02-19 13:41:47 +0100

    Update state.js

--- js/state.js
+++ js/state.js
@@ -1,3 +1,6 @@
-// Les variables partagées entre les fichiers
+// Les variables partagées entre les fichiers (La Mémoire globale)
 var scene, camera, renderer, controls, bottleGroup;
-var viewport3D; // Sera défini dans ui.js mais utilisé dans viewer.js
+var viewport3D;
+
+// NOUVEAU : On prépare la mémoire pour les gravures et la police 3D
+var font3D = null;
