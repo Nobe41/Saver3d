5c2119987acd4c9fbc1496493fd0bf47361f9cbb
Nobe <welcome.nobe@gmail.com>
2026-02-24 11:13:26 +0100

    state.js - rangement code 2

--- js/state.js
+++ js/state.js
@@ -1,6 +1,8 @@
 // Les variables partagées entre les fichiers (La Mémoire globale)
 var scene, camera, renderer, controls, bottleGroup;
 var viewport3D;
+var currentFileHandle = null; // Mémoire du projet ouvert
+var isLogicielInit = false; // Mémorise si le moteur 3D est lancé
 
 // Mémoire pour stocker les images (PNG) des gravures
 window.engravingImages = {};
